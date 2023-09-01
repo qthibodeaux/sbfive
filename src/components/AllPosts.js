33dee2fd7942f8b23acc26330da0d2df6209da24
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2023-08-31 23:46:36 -0500

    Playing around with post

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,7 +1,22 @@
-import { useParams } from "react-router-dom"
+import { useContext, useEffect, useState } from "react"
+import { Link, useParams } from 'react-router-dom'
+import { useAuth } from "../useAuth"
+import supabaseClient from "../supabaseClient"
 
 function AllPosts() {
     const { allpost, category } = useParams()
+    const { sess } = useAuth()
+    const [bumper, setBumper] = useState(0)
+    const [post, setPost] = useState(0)
+
+    useEffect(() => {
+        const queryPageNumber = category ? +category : 1;
+        supabaseClient
+            .rpc("get_posts", {page_number: queryPageNumber})
+            .select("*")
+    }, [sess, bumper, post])
+
+
     return (
         <div>
             AllPosts {allpost} {category}
